0ecb17fb4db2094a54c4567f0144dcbc33969a34
m <m@metaquanta.com>
2020-07-21 05:02:58 -0400

    beginnings of a sidebar

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,14 +1,41 @@
-import React, { MutableRefObject, useRef } from "react";
+import React, { CSSProperties, MutableRefObject, useRef, useState } from "react";
 
 export default function Sidebar() {
-  const hb: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const sb: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
+  const [state, setState] = useState({ visible: false, transitioning: false });
+
+  function buttonClicked() {
+    if (sb.current instanceof HTMLDivElement) {
+      setState({ visible: !state.visible, transitioning: true })
+    }
+  }
+
+  function sidebarTransitioned() {
+    setState({ visible: state.visible, transitioning: false })
+  }
+
+  const hiddenStyle: CSSProperties = {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    visibility: "hidden"
+  }
+  const transitioningStyle: CSSProperties = {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    visibility: "visible"
+  }
+  const visibleStyle: CSSProperties = {
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+    visibility: "visible"
+  }
   return (
     <div>
-      <div className="hamburger" ref={hb}>
-        ☰
+      <div className="hamburger" style={{
+        color: state.visible ? "rgb(100,100,100)" : "rgb(0,0,0)",
+        visibility: "visible"
+      }} onClick={buttonClicked} >
+        {state.visible ? '⊠' : '≡'}
       </div>
-      <div className="sidebar" />
+      <div className="sidebar" style={(!state.visible) ? state.transitioning ? transitioningStyle : hiddenStyle : visibleStyle} onTransitionEnd={sidebarTransitioned} ref={sb} />
     </div>
   );
 }
