307fcbacbba37cdedf3d9582cf50ab1ddcf183ea
m <m@metaquanta.com>
2020-07-21 15:30:05 -0400

    :( still borked

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,6 +1,9 @@
 import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Tiling } from "../classes/Tiling";
 import { Vec2 } from "../classes/Vec2";
 import { ViewPort } from "../classes/ViewPort";
+import { colorAngles, colorStreamer } from "../renderer/Colorer";
+import draw from "../renderer/DrawTile";
 import { Renderer } from "../renderer/Renderer";
 
 const getPosition = (canvas: HTMLCanvasElement): Vec2 => {
@@ -19,15 +22,15 @@ const getPosition = (canvas: HTMLCanvasElement): Vec2 => {
   return Vec2(0, 0);
 };
 
-const setPosition = (canvas: HTMLCanvasElement) => {
+function setPosition(canvas: HTMLCanvasElement) {
   const origin = getPosition(canvas);
   if (canvas.parentElement && canvas.parentElement.parentElement) {
     canvas.parentElement.style.top = `${origin.y}px`;
     canvas.parentElement.style.left = `${origin.x}px`;
   }
-};
+}
 
-const getSize = (canvas: HTMLCanvasElement): number => {
+function getSize(canvas: HTMLCanvasElement): number {
   if (canvas.parentElement && canvas.parentElement.parentElement) {
     console.log(
       `getSize(): ${window.screen.width}×${window.screen.height} - ${canvas.parentElement.style.width}×${canvas.parentElement.style.height} - ${canvas.width}×${canvas.height}`
@@ -35,9 +38,9 @@ const getSize = (canvas: HTMLCanvasElement): number => {
     return Math.round(Math.max(window.screen.height, window.screen.width));
   }
   return 1920;
-};
+}
 
-const setSize = (canvas: HTMLCanvasElement) => {
+function setSize(canvas: HTMLCanvasElement) {
   if (canvas.parentElement && canvas.parentElement.parentElement) {
     const size = getSize(canvas);
     canvas.parentElement.style.height = `${size}px`;
@@ -45,7 +48,7 @@ const setSize = (canvas: HTMLCanvasElement) => {
     canvas.height = Math.round(size * window.devicePixelRatio);
     canvas.width = Math.round(size * window.devicePixelRatio);
   }
-};
+}
 
 function getViewport(canvas: HTMLCanvasElement): ViewPort {
   const p = getPosition(canvas).invert().scale(window.devicePixelRatio);
@@ -60,38 +63,51 @@ function getViewport(canvas: HTMLCanvasElement): ViewPort {
   return ViewPort(Vec2(1920, 1080), p);
 }
 
-export default function Canvas(props: { renderer: Renderer }) {
-  console.log("Canvas()");
-  const el: MutableRefObject<HTMLCanvasElement | null> = useRef(null);
+class Canvas extends React.Component<
+  { tiling: Tiling; renderer: Renderer },
+  {}
+> {
+  canvasRef: MutableRefObject<HTMLCanvasElement | null>;
+  constructor(props: { tiling: Tiling; renderer: Renderer }) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
 
-  useEffect(() => {
-    console.log("Canvas.useEffect() [canvas/context]");
-    if (el.current instanceof HTMLCanvasElement) {
-      setSize(el.current);
-      setPosition(el.current);
-      props.renderer.setViewPort(getViewport(el.current));
-      const ctx = el.current.getContext("2d", { alpha: false });
+  resize() {
+    if (this.canvasRef.current) {
+      setPosition(this.canvasRef.current);
+    }
+  }
+
+  componentDidMount() {
+    this.resize.bind(this);
+    if (this.canvasRef.current) {
+      setSize(this.canvasRef.current);
+      setPosition(this.canvasRef.current);
+      window.addEventListener("resize", this.resize);
+      const ctx = this.canvasRef.current.getContext("2d");
       if (ctx) {
-        props.renderer.setContext(ctx);
+        const v = Vec2(20, 30);
+        const u = Vec2(1000, 500);
+        this.props.renderer.setContext(ctx);
+        this.props.renderer.setViewPort(getViewport(this.canvasRef.current));
+        //this.props.renderer.setfillColorer(colorStreamer(colorAngles(4, 2, 50, 50))());
+        this.props.renderer.setDrawTiles(draw);
       }
     }
-  });
+  }
 
-  const update = () => {
-    if (el.current instanceof HTMLCanvasElement) {
-      setPosition(el.current);
-    }
-  };
+  render() {
+    console.log("Canvas()");
 
-  useEffect(() => {
-    console.log("Canvas.useEffect() [resize]");
-    window.addEventListener("resize", update);
-    return () => window.removeEventListener("resize", update);
-  });
+    //getViewport(el.current));
 
-  return (
-    <div className="canvas">
-      <canvas ref={el} />
-    </div>
-  );
+    return (
+      <div className="canvas">
+        <canvas ref={this.canvasRef} />
+      </div>
+    );
+  }
 }
+
+export default Canvas;
